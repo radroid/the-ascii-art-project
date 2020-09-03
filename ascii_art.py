46f1acffe0fa5a559abd33f57cfd47f18bab37db
radroid <raj9dholakia@gmail.com>
2020-09-03 21:44:27 +0530

    Update files - Add one function that can create art from an image. - Add a picture of me. - Update gitignore.

--- ascii_art.py
+++ ascii_art.py
@@ -7,12 +7,16 @@ from PIL import Image
 import numpy as np
 import re
 from time import time
+import os
 
 
 class ASCIIArt:
     """Takes an image and prints it using ASCII on the shell terminal."""
 
-    def __init__(self, path_to_image: str or pathlib.PosixPath = None):
+    CHAR_FACTOR = 3
+    STD_HEIGHT = 300
+
+    def __init__(self, path_to_image: str or pathlib.PosixPath = None,):
         if path_to_image is None:
             path_to_image = 'images/ascii-pineapple.jpg'
 
@@ -21,8 +25,8 @@ class ASCIIArt:
         self.arr_image = self.convert_to_array(self.image)
         self.ascii_art = None
 
-        print(f'Image "{self.path_to_image.name}" has been successfully \
-            loaded!\n')
+        print(f'Image "{self.path_to_image.name}" has been successfully '
+              'loaded!\n')
         print(f'Some image metadata: \
             \n- Size: {self.image.size[0]} x {self.image.size[1]} \
             \n- Format: {self.image.format} \
@@ -50,8 +54,8 @@ class ASCIIArt:
             path = pathlib.Path(path)
 
         if not path.exists():
-            raise FileNotFoundError(f'The path provided, does not exist.\n \
-                                    path: {path}')
+            raise FileNotFoundError(f'The path provided, does not exist. \
+                                    \npath: {path}')
         if not path.is_file():
             raise TypeError(f'No image file present at {path}')
 
@@ -78,7 +82,12 @@ class ASCIIArt:
         if re.match(pattern, string) is None:
             raise TypeError('The provided argument is not a PIL class.')
 
-        return np.array(image)
+        # Resize the image to meet standard
+        h = ASCIIArt.STD_HEIGHT
+        ratio = image.size[0] / image.size[1]
+        resized_im = image.resize((int(round(ratio * h)), h),
+                                  Image.ANTIALIAS)
+        return np.array(resized_im)
 
     @staticmethod
     def convert_to_image(array):
@@ -121,10 +130,22 @@ class ASCIIArt:
         return b_matrix
 
     def convert_to_ascii_char(self):
+        """Converts the brightness matrix into a string of ASCII charaters
+         that can be printed on a terminal window to create an image.
+
+        Returns:
+            str: a string of ASCII characters that when printed will create
+                 the picture in terminal.
+        """
         chars = "\
         `^\",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$"
 
-        def ascii_scaler(x, min=0, max=255, chars=chars.strip()):
+        b_matrix = self.create_brightness_matrix()
+
+        minimum = b_matrix.min()
+        maximum = b_matrix.max()
+
+        def average(x, min=minimum, max=maximum, chars=chars.strip()):
             """Normalises value of x according to the min and max values.
 
             Args:
@@ -136,19 +157,79 @@ class ASCIIArt:
                 int: normalised integer value of x.
             """
             value = (x - min) / (max - min) * (len(chars) - 1)
-            return chars[int(round(value))]
+            return chars[int(round(value))] * self.CHAR_FACTOR
 
-        b_matrix = self.create_brightness_matrix()
+        def funcname(self, parameter_list):
+            pass
+
+        ascii_scaler = average
+        t0 = time()
         ascii_array = np.vectorize(ascii_scaler)(b_matrix)
+        print(f'Time for ASCII conversion: {round(time() - t0, 3)} s\n')
+
         ascii_art = ''.join([''.join(row) for row in ascii_array.tolist()])
         self.ascii_art = ascii_art
         return ascii_art
 
+    def print_ascii_art(self):
+        height, width, RBG = self.arr_image.shape
+        os.system(f"printf '\\e[8;{height};{width * self.CHAR_FACTOR}t'")
+        print(self.ascii_art)
+
+
+def create_ascii_art():
+    """Takes user input and prints ASCII Art of the picture."""
+    print('Hi! I will guide you through how you could get that picture '
+          'printed in this terminal window. It is going to be made of ASCII '
+          'characters!\n\nIf you get some spare time, I would love to know '
+          'what you think of this project. Any kind of feedback is welcome.\n')
+
+    print('\n* Start by going to the project folder and then "images" folder. '
+          'Add any picture you would like to create art out of into this '
+          'folder.')
+
+    input('\nPress ENTER when you are done.')
+
+    filename = input('\nEnter the name of the picture file with '
+                     'its extension: ')
+    art = None
+
+    while True:
+        try:
+            path = pathlib.Path('images') / filename
+            art = ASCIIArt(path)
+            break
+        except FileNotFoundError as error:
+            print(error)
+            filename = input('\nDid you forget to add the picture file into '
+                             'the "images" folder? \n'
+                             'Please re-enter a valid filename: ')
+        except TypeError as error:
+            print(error)
+            filename = input('\nDid you forget to add the picture file into '
+                             'the "images" folder? \n'
+                             'Please re-enter a valid filename: ')
+
+    art.convert_to_ascii_char()
+
+    print('\nPlease zoom out of terminal now. ("cmd" + "-") \n\nDETAILS: the '
+          'image can be bigger than the maximum size of the current terminal '
+          'window. Hence, it is better to zoom out as much as you can before '
+          'the image is printed. \nREMEMBER: use "clear" command or close the '
+          'window if you want to do it again.')
+
+    input('\n\nPress ENTER when you have zoomed out enough to not read this '
+          'text. * Zoom back in after typing "clear".')
+
+    art.print_ascii_art()
+
 
 if __name__ == '__main__':
-    print('Welcome to the ASCII Art Project\n')
-    art = ASCIIArt()
-    ascii_art = art.convert_to_ascii_char()
-    print(ascii_art)
+    print('\nWelcome to the ASCII Art Project\n\n')
+    create_ascii_art()
+    # path = pathlib.Path('images') / 'krys.jpeg'
+    # art = ASCIIArt(path)
+    # art.convert_to_ascii_char()
+    # art.print_ascii_art()
     # art.image.show()
     # art.create_brightness_matrix()
